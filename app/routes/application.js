f389f2ee83ec63d6a5f1aab6c78fdf9ede94a9b6
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2020-09-28 16:06:15 +0100

    Eda spike receive application (#8)
    * Update format of minimal application details from frontend EOI

--- app/routes/application.js
+++ app/routes/application.js
@@ -14,16 +14,18 @@ module.exports = [
     options: {
       validate: {
         payload: Joi.object({
+          businessName: Joi.string().required(),
           confirmationId: Joi.string().required(),
-          cost: Joi.string().required(),
-          userId: Joi.string().required()
+          emailAddress: Joi.string().required(),
+          inEngland: Joi.boolean().required()
         }),
         failAction: (request, h) => h.response('Error in payload data').code(400).takeover()
       },
       handler: (request, h) => {
         console.log('New application received:')
-        console.log(`UserID: ${request.payload.userId}`)
-        console.log(`Cost: ${request.payload.cost}`)
+        console.log(`In England: ${request.payload.inEngland}`)
+        console.log(`Business Name: ${request.payload.businessName}`)
+        console.log(`Email Address: ${request.payload.emailAddress}`)
         console.log(`ConfirmationID: ${request.payload.confirmationId}`)
 
         return h.response(`Created application with confirmation is ${request.payload.confirmationId}`).code(201)
