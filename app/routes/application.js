eb338856156b4f5899631de58bf51dda7bc87e83
Paul Andrews <paul.andrews@defra.gov.uk>
2020-10-01 22:35:22 +0100

    Return application details on GET request

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,25 @@
 const Joi = require('joi')
+const { app } = require('../server')
 const Application = require('../models').Application
 
 module.exports = [
   {
     method: 'GET',
     path: '/application',
-    handler: (request, h) => {
-      return h.response('Dummy response').code(200)
+    handler: async (request, h) => {
+      if (!request.query.confirmationId) {
+        return h.response('No confirmation ID provided').code(400)
+      }
+
+      const application = await Application.findOne({
+        where: { confirmationId: request.query.confirmationId }
+      })
+
+      if (!application) {
+        return h.response(`No application with confirmation ID ${request.query.confirmationId}`).code(400)
+      }
+
+      return h.response(application.toJSON()).code(200)
     }
   },
   {
@@ -36,7 +49,7 @@ module.exports = [
           inEngland: request.payload.inEngland
         })
 
-        return h.response(`Created application with confirmation is ${request.payload.confirmationId}`).code(201)
+        return h.response(`Created application with confirmation ID ${request.payload.confirmationId}`).code(201)
       }
     }
   }
