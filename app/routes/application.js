4c60b316f96d7e8f429ee91295889497a2eebe0d
Paul Andrews <paul.andrews@defra.gov.uk>
2020-10-01 19:53:59 +0100

    Write new application to database

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,5 @@
 const Joi = require('joi')
+const Application = require('../models').Application
 
 module.exports = [
   {
@@ -21,12 +22,19 @@ module.exports = [
         }),
         failAction: (request, h) => h.response('Error in payload data').code(400).takeover()
       },
-      handler: (request, h) => {
+      handler: async (request, h) => {
         console.log('New application received:')
         console.log(`In England: ${request.payload.inEngland}`)
         console.log(`Business Name: ${request.payload.businessName}`)
         console.log(`Email Address: ${request.payload.emailAddress}`)
         console.log(`ConfirmationID: ${request.payload.confirmationId}`)
+
+        await Application.create({
+          confirmationId: request.payload.confirmationId,
+          businessName: request.payload.businessName,
+          emailAddress: request.payload.emailAddress,
+          inEngland: request.payload.inEngland
+        })
 
         return h.response(`Created application with confirmation is ${request.payload.confirmationId}`).code(201)
       }
--- app/server.js
+++ app/server.js
@@ -1,15 +1,9 @@
 const Hapi = require('@hapi/hapi')
-const models = require('./models')
-
-async function showApps (application) {
-  const apps = await application.findAll()
-  console.log('All apps: ', JSON.stringify(apps, null, 2))
-}
-
-showApps(models.Application)
 
 const server = Hapi.server({
   port: process.env.PORT
 })
+
+server.route(require('./routes'))
 
 module.exports = server
